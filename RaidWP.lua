37919e95f88da71da79b42b664c7c04b8421d35a
xlostpexz <98389349+xlostpexz@users.noreply.github.com>
2022-05-14 09:56:22 +0700

    Update and rename README.md to RaidWP.lua

---
+++ RaidWP.lua
@@ -0,0 +1,64 @@
+_G.Raid = true
+
+    local placeId = game.PlaceId
+	if placeId == 9487450292 then
+		First = true
+	elseif placeId == 9609355203 then
+		Raid = true
+	end
+	
+    spawn(function()
+       game:GetService("RunService").RenderStepped:Connect(function()
+        pcall(function()
+            if _G.Raid then
+                if First then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(34.6641273, 95.6252594, 726.025513, 0.485765338, -2.5060026e-06, 0.874089241, -0.000205610399, 1, 0.000117132731, -0.874089241, -0.000236620966, 0.485765338)
+end
+            end
+        end)
+       end)
+    end)
+    
+    spawn(function()
+       game:GetService("RunService").RenderStepped:Connect(function()
+        pcall(function()
+            if _G.Raid then
+                if Raid then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-6.69188023, 200.026367, -14.0853148, -0.981415331, -0.19189553, 2.52705613e-05, -2.32650541e-06, 0.000143587633, 1, -0.191895545, 0.981415331, -0.000141365555)
+end
+            end
+        end)
+       end)
+    end)
+    
+    spawn(function()
+        game:GetService("RunService").Heartbeat:Connect(function()
+            if game:GetService("Players").LocalPlayer.Character:FindFirstChild("Humanoid") and _G.Raid then
+                setfflag("HumanoidParallelRemoveNoPhysics", "False")
+                setfflag("HumanoidParallelRemoveNoPhysicsNoSimulate2", "False")
+                game:GetService("Players").LocalPlayer.Character.Humanoid:ChangeState(11)
+            end
+        end)
+    end)
+
+        spawn(function()
+       game:GetService("RunService").RenderStepped:Connect(function()
+        pcall(function()
+            if _G.Raid then
+            game:GetService'VirtualUser':CaptureController()
+            game:GetService'VirtualUser':Button1Down(Vector2.new(1280, 672))
+            end
+        end)
+       end)
+        end)
+        
+            spawn(function()
+       game:GetService("RunService").RenderStepped:Connect(function()
+        pcall(function()
+            if _G.Raid then
+                wait(1)
+game.Players.LocalPlayer.Character.Humanoid:EquipTool(game:GetService("Players").LocalPlayer.Backpack:FindFirstChild(_G.Weapon))
+            end
+        end)
+       end)
+    end)
